a41118798d91e2987fbe3e8fa364e44c286a0e68
Abhishek Yadav <49701194+abhishekyadavindia@users.noreply.github.com>
2020-07-10 12:03:42 +0530

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,5 +1,8 @@
 <?php
     //  Here we are making connection with database having server localhost, username root, password 123456, and in database we are using MySQL Database in which we have database name test and it has columns Email, Password, Name, dt, Address.
+    // Author- Abhishek Yadav
+    // Please visit - https://abhishekyadavindia.blogspot.com
+    // Please also visit my youtube channel - https://www.youtube.com/channel/UCpWAaAz70iWB5h-BayZPqig
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '123456');
