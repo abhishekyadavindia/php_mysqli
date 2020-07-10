2848d09c6cf0c37d7b026109535d799a5f9cd33e
Abhishek Yadav <49701194+abhishekyadavindia@users.noreply.github.com>
2020-07-10 12:04:36 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,4 +1,8 @@
 <?php
+
+  // Author- Abhishek Yadav
+  // Please visit - https://abhishekyadavindia.blogspot.com
+  // Please also visit my youtube channel - https://www.youtube.com/channel/UCpWAaAz70iWB5h-BayZPqig
   session_start();
    if (isset($_SESSION['loggedin'])) {
      header("location: /php_video/login/welcome.php");
