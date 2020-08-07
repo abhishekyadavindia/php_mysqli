9ad7c95ae84f7aa5614a3eae485534661bd576af
Abhishek Yadav <49701194+abhishekyadavindia@users.noreply.github.com>
2020-08-07 12:57:57 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,28 @@
+<?php
+// PHP in one video tutorial.
+/*
+This
+is 
+multiline
+comment
+*/
+// PHP-HyperText Preprocessor
+echo "Variables in PHP <br>";
+$var1 = 6;
+$var2 = 3;
+echo "Variables are ";echo $var1, $var2;echo "<br>";
+echo "Operators in PHP<br>";
+echo $var1 + $var2;echo "<br>";
+echo $var1 - $var2;echo "<br>";
+echo $var1 * $var2;echo "<br>";
+echo $var1 / $var2;echo "<br>";
+echo $var1 % $var2;echo "<br>";
+echo $var1 ** $var2;echo "<br>";
+echo "Associative Operators in PHP";echo "<br>";
+$var1 = $a;
+echo $a;
+
+
+
+?>
+<!-- </html></> -->
