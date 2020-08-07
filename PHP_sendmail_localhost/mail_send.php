78eee806a801ca4b8dcb6cfa9f16ded98cf0c809
Abhishek Yadav <49701194+abhishekyadavindia@users.noreply.github.com>
2020-08-07 13:13:50 +0530

    Update mail_send.php

--- PHP_sendmail_localhost/mail_send.php
+++ PHP_sendmail_localhost/mail_send.php
@@ -2,10 +2,12 @@
 // Using mail function to mail any GMail ID
 // We are using SMTP-Simple Mail Transfer Protocol.
 // SMTP of Google is free service which is used to send and recieve gmail using there service.
-$to_email = "abhishekyadav95869@gmail.com";
+// Enter Email of Receiver.
+$to_email = "youremail@gmail.com";
 $subject = "iCode";
-$body = "Hello Abhishek welcome to iCode";
-$headers = "From: noreplyicodeilearn@gmail.com";
+$body = "Hello Welcome Please Check Mail.";
+// Please Enter Email from where you have to send it.
+$headers = "From: youremail@gmail.com";
 if (mail($to_email, $subject, $body, $headers)) {
     echo "Mail to " . $to_email . " is send successfully";
 }
