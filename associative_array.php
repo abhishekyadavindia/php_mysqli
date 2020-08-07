32fe7585e1100b906ab2f2c39330c8b72742d0a4
Abhishek Yadav <49701194+abhishekyadavindia@users.noreply.github.com>
2020-08-07 18:30:28 +0530

    Add files via upload

---
+++ associative_array.php
@@ -0,0 +1,7 @@
+<?php
+// Associative Array
+// To Create Associative Array use array function of php.
+// To define objects of that array we use double arrow operator(=>).
+$age = array("Peter"=>"35", "Ben"=>"20", "Joe"=>"10");
+echo "Peter is " . $age['Peter'] . " age years old."
+?>
